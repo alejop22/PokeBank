0adad6924d51e8d8fd5dae2dff3bb3613d154cb5
Alejandro Palacio <alejandrooss94@gmail.com>
2021-01-11 16:06:13 -0500

    Consumo de la API de Pokemon

--- src/index.js
+++ src/index.js
@@ -33,3 +33,24 @@ retira.addEventListener('click', () => {
     };
 });
 
+//API//
+const API = 'https://pokeapi.co/api/v2/pokemon/';
+let buttonAPI = document.getElementById('btnAPI');
+
+const numeroAleatorio = () => {
+    return Math.floor(Math.random()*((850+1)-1)+1);
+};
+
+buttonAPI.addEventListener('click', () => {
+    let img = document.getElementById('img');
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("GET",`${API}${numeroAleatorio()}`);
+    xhttp.send();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            let datoPokemon = JSON.parse(this.responseText);
+            img.setAttribute("src",datoPokemon.sprites.front_default)
+        }
+    };
+});
